ba1c630920f3cce4050fa97e40eab8b095b07fa1
tomasbutkevicius <tomas.butkevicius99@gmail.com>
2020-02-24 17:49:01 +0200

    started with spring

---
+++ demo-server/src/main/java/com/swedbank/academy/demoserver/DemoServerApplication.java
@@ -0,0 +1,15 @@
+package com.swedbank.academy.demoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.swedbank.academy.demoserver.person"})
+public class DemoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServerApplication.class, args);
+	}
+
+}
